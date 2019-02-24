0a197cef11eb0071ce6e31e4a169d4b9c0b35f6b
Jake Bezold <crackerjake@optonline.net>
2019-02-24 08:51:41 -0500

    wip

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from config import Config
-
+import os, requests
+proxyDict = {
+              "http"  : os.environ.get('FIXIE_URL', ''),
+              "https" : os.environ.get('FIXIE_URL', '')
+            }
+r = requests.get('http://www.example.com', proxies=proxyDict)
 
 app = Flask(__name__)
 app.config.from_object(Config)
