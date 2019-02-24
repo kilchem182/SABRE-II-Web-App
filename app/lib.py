648580040dc4d690b01a230697851a39aadc9b74
kilchem182 <32198896+kilchem182@users.noreply.github.com>
2019-02-24 07:07:01 -0500

    Integrated Database

--- app/lib.py
+++ app/lib.py
@@ -1,15 +1,38 @@
 import json
 import requests
+import pymongo
 
-def dictToJSON(file, dict): #json.dumps() puts dict in to json, json.loads() puts json in to dict
-    with open(file, 'w') as file:
-        json.dump(dict, file)
+def initDatabase():
+    client = pymongo.MongoClient("mongodb+srv://kilchem182:sabreiitcnj123@sabreii-database-b6sjy.mongodb.net/test?retryWrites=true")
+    db = client["database"]
+    col = db["jukeBoxCollection"]
+    post = {"jukeBox": [{"trackId": 290884853, "artistName": "X JAPAN", "collectionName": "Blue Blood", "trackName": "X", "artworkUrl100": "https://is4-ssl.mzstatic.com/image/thumb/Music/v4/a7/5c/81/a75c818e-9f2b-0571-63f0-ec7a3fa7aa86/source/100x100bb.jpg", "vote": 0}]}
+    _id = col.insert_one(post)
+    return
 
-def JSONToDict(file):#converts JSON style data to a dictionary
-    with open(file) as file:
-        jsonString = file.read()
-        jsonInfo = json.loads(jsonString)
-        return jsonInfo
+def getID():
+    client = pymongo.MongoClient("mongodb+srv://kilchem182:sabreiitcnj123@sabreii-database-b6sjy.mongodb.net/test?retryWrites=true")
+    db = client["database"]
+    cursor = db.jukeBoxCollection
+    for doc in cursor.find():
+        return(doc['_id'])
+
+def dictToJSON(id, dict): #json.dumps() puts dict in to json, json.loads() puts json in to dict
+    client = pymongo.MongoClient("mongodb+srv://kilchem182:sabreiitcnj123@sabreii-database-b6sjy.mongodb.net/test?retryWrites=true")
+    db = client["database"]
+    col = db["jukeBoxCollection"]
+    newVals = {"$set" : dict}
+    col.update_one({'_id': id}, newVals)
+    return
+
+def JSONToDict(id):#converts JSON style data to a dictionary
+    client = pymongo.MongoClient("mongodb+srv://kilchem182:sabreiitcnj123@sabreii-database-b6sjy.mongodb.net/test?retryWrites=true")
+    db = client["database"]
+    col = db["jukeBoxCollection"]
+    query = {'_id': id}
+    result = col.find(query)
+    for obj in result:
+        return {"jukeBox" : obj["jukeBox"]}
 
 def addSongToArray(songList, newSong):
     jukeBox = songList["jukeBox"]
@@ -52,17 +75,22 @@ def PrioritizeList(songList):
     return songList
 
 '''
-songList = [{'Test1': 1, 'Test2': 2}]
-newSong = {'Test3': 3, 'Test4': 4}
-print(addSongToArray(songList, newSong))
-
-songs = {
-            0: 'Party in the U.S.A',
-            1: 'Tunak Tunak Tun',
-            2: 'Gangnam Style'
-}
-addSongToQ(songs)
-val = loadJSON()
-print(val[3])
-addSongToQ(val)
+initDatabase()
+id = getID()
+print(id)
+songList = JSONToDict(id)
+print(songList)
+songList = addSongToArray(songList, {"Test" : 1})
+print(songList)
+dictToJSON(id, songList)
+songList = JSONToDict(id)
+print(songList)
+songList = addSongToArray(songList, {"Test2" : 2})
+print(songList)
+dictToJSON(id, songList)
+songList = JSONToDict(id)
+print(songList)
+songList = addSongToArray(songList, {"Test3" : 3})
+print(songList)
+dictToJSON(id, songList)
 '''
